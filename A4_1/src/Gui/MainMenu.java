d90dc7fdfb7f6c734356f5982ac65ff2ef71f45f
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-03-04 19:12:47 +0000

    Start

---
+++ A4_1/src/Gui/MainMenu.java
@@ -0,0 +1,17 @@
+package Gui;
+
+import javax.swing.JFrame;
+
+public class MainMenu extends JFrame {
+
+	private static final long serialVersionUID = -7861041191114861059L;
+
+	public MainMenu() {
+		super("Main Menu");
+		
+		setSize(600, 600);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+}
---
+++ A4_1/src/Logic/Main.java
@@ -0,0 +1,11 @@
+package Logic;
+
+import Gui.MainMenu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new MainMenu();
+	}
+	
+}
