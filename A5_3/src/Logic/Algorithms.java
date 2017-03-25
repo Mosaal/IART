23d3a55f115355273b26ac1d29599b86f542ba9a
JoaoIsaias <jl.isaias123@gmail.com>
2017-03-25 18:33:52 +0000

    Corrigi o errozito e adaptei o algoritmo para fazer o DFS (deve de estar bem)

--- A5_3/src/Logic/Algorithms.java
+++ A5_3/src/Logic/Algorithms.java
@@ -9,7 +9,63 @@ import Game.*;
 public class Algorithms {
 
 	public static Move[] DFS(Board board) {
-		
+		//Move[] moves = null;
+		List<Board> to_visit = new ArrayList<Board>();
+		List<Board> visited = new ArrayList<Board>();
+		to_visit.add(board);
+		visited.add(board);
+		int aux = 0;
+		while(!to_visit.isEmpty()) {
+			for (Entry<Integer, Block> block: to_visit.get(aux).getBlocks().entrySet()) {
+				if(to_visit.get(aux).canBlockBeMoved(block.getValue(), 0)) {
+					to_visit.get(aux).moveBlock(block.getValue(), 0);
+					if(!visited.contains(to_visit.get(aux))){
+						to_visit.add(1,to_visit.get(aux));	
+						visited.add(to_visit.get(aux));
+						//adicionar ao moves
+						if(to_visit.get(aux).wingame()){
+							//return moves;
+						}
+					}
+				}
+				if(to_visit.get(aux).canBlockBeMoved(block.getValue(), 1)) {
+					to_visit.get(aux).moveBlock(block.getValue(), 1);
+					if(!visited.contains(to_visit.get(aux))){
+						to_visit.add(1,to_visit.get(aux));	
+						visited.add(to_visit.get(aux));
+						//adicionar ao moves
+						if(to_visit.get(aux).wingame()){
+							//return moves;
+						}
+					}
+				}
+				if(to_visit.get(aux).canBlockBeMoved(block.getValue(), 2)) {
+					to_visit.get(aux).moveBlock(block.getValue(), 2);
+					if(!visited.contains(to_visit.get(aux))){
+						to_visit.add(1,to_visit.get(aux));
+						visited.add(to_visit.get(aux));
+						//adicionar ao moves
+						if(to_visit.get(aux).wingame()){
+							//return moves;
+						}
+					}
+				}
+				if(to_visit.get(aux).canBlockBeMoved(block.getValue(), 3)) {
+					to_visit.get(aux).moveBlock(block.getValue(), 3);
+					if(!visited.contains(to_visit.get(aux))){
+						to_visit.add(1,to_visit.get(aux));	
+						visited.add(to_visit.get(aux));
+						//adicionar ao moves
+						if(to_visit.get(aux).wingame()){
+							//return moves;
+						}
+					}
+				}	
+				to_visit.remove(0);
+				aux++;
+			}
+			aux=0;
+		}		
 		return null;
 	}
 	
@@ -26,7 +82,7 @@ public class Algorithms {
 		List<Board> visited = new ArrayList<Board>();
 		to_visit.add(board);
 		visited.add(board);
-		int aux= 0;
+		int aux = 0;
 		while(!to_visit.isEmpty()) {
 			for (Entry<Integer, Block> block: to_visit.get(aux).getBlocks().entrySet()) {
 				if(to_visit.get(aux).canBlockBeMoved(block.getValue(), 0)) {
@@ -73,6 +129,7 @@ public class Algorithms {
 						}
 					}
 				}	
+				to_visit.remove(0);
 				aux++;
 			}
 			aux=0;
